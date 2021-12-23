41fe1d62531ea8d068cdd950b7e1a89e993aec9d
Seun Oshitade <seun.oshitade@gmail.com>
2021-12-23 13:51:50 +0000

    Accessed and manipulated the event that trigerred an event

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const numberOfButtons = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", () => {
-    alert("I got clicked!");
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    this.style.color = "white";
   })
 }
+
+// const audio = new Audio('sounds/tom-4.mp3');
+// audio.play();
